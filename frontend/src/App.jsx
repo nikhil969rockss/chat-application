d2732200791cefe8f7e80e9ef2a821e3465ce617
nikhil969rockss <ns9013604867@gmail.com>
2025-07-17 23:29:01 +0530

    initial setup and some ui compontents with routing enabled

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Button } from "./components/ui/button";
+
+//pages
+import Auth from "./pages/auth/Auth";
+import Chat from "./pages/chat/Chat";
+import Profile from "./pages/profile/Profile";
+
+    return (
+        <>
+            <Routes>
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="*" element={<Navigate to={"/auth"} />} />
+            </Routes>
+        </>
+    );
+};
+export default App;
